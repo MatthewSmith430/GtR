817f0363fefe1a79c5ae44a3d60e1846581eddf6
Matthew Smith <matt_smith.430@tiscali.co.uk>
2020-11-04 11:50:36 +0000

    add_scimago_data

---
+++ R/JOURNAL_DATA_data.R
@@ -0,0 +1,8 @@
+#' @title JOURNAL_DATA
+#'
+#' @description Dataframe of SJR and H-Inex for journals (extracted from Scimago)
+#' @name JOURNAL_DATA
+#' @docType data
+#' @usage JOURNAL_DATA
+#' @keywords datasets
+NULL
